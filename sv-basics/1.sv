895fca2c8fe8b98cff36a2056fe670e8859f5b88
Om Tank <152820437+OTANK10@users.noreply.github.com>
2025-06-25 14:39:35 -0400

    Create 1.sv

---
+++ sv-basics/1.sv
@@ -0,0 +1,52 @@
+`timescale 1ns / 1ps
+
+module testbench();
+  
+
+ /////global signals
+  
+  reg clk;
+  reg reset;
+  
+  reg [3:0] temp;
+  
+  initial begin
+    clk = 1'b0;//single bit binary zero
+    reset = 1'b0;
+  
+  end
+ 
+  initial begin
+    reset = 1b'1;
+    #30;
+    reset = 1b'0;
+  end
+  
+  initial begin
+    temp = 4'b0100; // 4 bits of temp in binary
+    #10;
+    temp = 4'b1100;
+    #10;
+    temp = 4'b0011;
+    #10;
+  end
+  
+  
+  initial begin
+  	$dumpfile("dump.vcd");
+  	$dumpvars;
+  end
+  
+  initial begin
+    $monitor("Temp : %0d at time : %0t",temp, $time);
+  end
+  
+  initial begin
+    #200;
+    $finish();
+    
+  end
+  
+  
+endmodule
+  
