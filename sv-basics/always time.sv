a8176558d733d21ffdab5681cfed95059153dfc3
Om Tank <152820437+OTANK10@users.noreply.github.com>
2025-06-25 21:38:16 -0400

    Update always time.sv

--- sv-basics/always time.sv
+++ sv-basics/always time.sv
@@ -1,52 +1,48 @@
-`timescale 1ns /1ps
-
-module testbench();
+`timescale 1ns / 1ps
  
-  reg clk;
-  reg rst;
+module tb();
+ 
   
-  reg clk50 = 0;
+  reg clk; //initial value = X
   
-  reg clk25 = 0;
+  reg clk50;
+  reg clk25 = 0;  ///initialize variable
   
+ 
   initial begin
     clk = 1'b0;
-    rst = 1'b0;
+    clk50 = 0; 
   end
-  
-  //always #5 clk = ~clk; //clk operating at 100mhz
-  
- // always #10 clk50 = ~clk50; //50mhz
-  
-  //always #20 clk25 = ~clk25; //25mhz
-  
+ 
+ 
   
   always #5 clk = ~clk;
   
   always begin
-    #5;
-    clk50 = 1;
-    #10;
-    clk50 = 0;
-    #5;
+  #5;
+   clk50 = 1;
+  #10;
+   clk50 = 0; 
+   #5; 
   end
   
   always begin
     #5;
     clk25 = 1;
-    #5;
+    #20;
     clk25 = 0;
+    #15;  
   end
-  
-  
+ 
   initial begin
     $dumpfile("dump.vcd");
     $dumpvars;
   end
-
- initial begin
-   #200;
-   $finish();
- end
+ 
+ 
+  initial begin
+    #200;
+    $finish();
+  end
   
 endmodule
